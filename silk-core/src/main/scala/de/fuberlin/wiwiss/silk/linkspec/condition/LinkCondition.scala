ec1ce5d935865abc861f0902adc8907e1f5da06a
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2011-04-01 13:33:08 +0200

    introduced bigint indexing

--- silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/condition/LinkCondition.scala
+++ silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/condition/LinkCondition.scala
@@ -38,30 +38,52 @@ case class LinkCondition(rootOperator : Option[Operator])
    *
    * @return A set of (multidimensional) indexes. Instances within the threshold will always get the same index.
    */
-  def index(instance : Instance, threshold : Double) : Set[Int] =
+  def index(instance : Instance, threshold : Double) : Set[BigInt] =
   {
-    rootOperator match
+    val instanceIndices:Set[BigInt] = rootOperator match
     {
       case Some(operator) =>
       {
         val indexes = operator.index(instance, threshold)
+        val bigIndexes = indexes.map(_.map(BigInt(_)))
 
+        if (instance.uri == "http://rdf.tripwolf.com/tw.locations/9687"){
+          System.out.println("--indices: " + bigIndexes)
+          //System.out.println("--blocks: " + blocks)
+        }
         //Convert the index vectors to scalars
-        for(index <- indexes) yield
+        for(index <- bigIndexes) yield
         {
-          (index zip operator.blockCounts).foldLeft(0){case (iLeft, (iRight, blocks)) => {
+          (index zip operator.blockCounts).foldLeft(BigInt(0)){case (iLeft:BigInt, (iRight, blocks)) => {
               var blocksToUse = blocks
               if (blocksToUse == 0) blocksToUse=1;
-              iLeft * blocksToUse + iRight
+              val res = iLeft * blocksToUse + iRight
+              if (instance.uri == "http://rdf.tripwolf.com/tw.locations/9687"){
+                System.out.println("blocks: " + blocks + ", iRight: " + iRight + " intermediate result: " + res)
+              }
+              res
             }
           }
-        }
+        }:BigInt
       }
       case None => Set.empty
     }
-
+    if (instance.uri == "http://rdf.tripwolf.com/tw.locations/9687"){
+      System.out.println("instance indices:" + instanceIndices)
+    }
+//    val blockIndices = for (index <- instanceIndices) yield {
+//      val blockIndex = (index % blocks).toInt
+//      if (blockIndex < 0 ) {
+//        blockIndex + blocks
+//      } else {
+//        blockIndex
+//      }
+//    }
+//    if (instance.uri == "http://rdf.tripwolf.com/tw.locations/9687"){
+//      System.out.println("block indices: " + blockIndices)
+//    }
+    instanceIndices
   }
-
   /**
    * The number of blocks in each dimension of the index.
    */
