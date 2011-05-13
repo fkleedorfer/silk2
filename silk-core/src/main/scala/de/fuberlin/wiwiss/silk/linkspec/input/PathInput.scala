715e866df4e10c750709d2b422c36f66c2790488
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2011-05-13 17:12:42 +0200

    Added support for paths without pathOperators (e.g., '?a') this is useful for comparisons like '?a/owl:sameAs' '?b'

--- silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/input/PathInput.scala
+++ silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/input/PathInput.scala
@@ -20,29 +20,45 @@ case class PathInput(path : Path) extends Input
    */
   override def apply(instances : SourceTargetPair[Instance]) =
   {
-    if(pathIndex == -1)
-    {
-        if(instances.source.spec.variable == path.variable)
-        {
-          pathIndex = instances.source.spec.pathIndex(path)
-        }
-        else if(instances.target.spec.variable == path.variable)
-        {
-          pathIndex = instances.target.spec.pathIndex(path)
-        }
-    }
+    if (path.operators.isEmpty){
+      //the path only consists of ?[variable]
+      if(instances.source.spec.variable == path.variable)
+      {
+        Set(instances.source.uri)
+      }
+      else if(instances.target.spec.variable == path.variable)
+      {
+        Set(instances.target.uri)
+      }
+      else
+      {
+        Traversable.empty
+      }
+    } else {
+      if(pathIndex == -1)
+      {
+          if(instances.source.spec.variable == path.variable)
+          {
+            pathIndex = instances.source.spec.pathIndex(path)
+          }
+          else if(instances.target.spec.variable == path.variable)
+          {
+            pathIndex = instances.target.spec.pathIndex(path)
+          }
+      }
 
-    if(instances.source.spec.variable == path.variable)
-    {
-      instances.source.evaluate(pathIndex)
-    }
-    else if(instances.target.spec.variable == path.variable)
-    {
-      instances.target.evaluate(pathIndex)
-    }
-    else
-    {
-      Traversable.empty
+      if(instances.source.spec.variable == path.variable)
+      {
+        instances.source.evaluate(pathIndex)
+      }
+      else if(instances.target.spec.variable == path.variable)
+      {
+        instances.target.evaluate(pathIndex)
+      }
+      else
+      {
+        Traversable.empty
+      }
     }
   }
 
